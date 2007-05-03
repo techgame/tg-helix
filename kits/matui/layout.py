37a61747a700d129871d559bdb3d92d2a43dce76
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2007-05-03 00:07:46 -0600

    Refined layouts through use

--- kits/matui/layout.py
+++ kits/matui/layout.py
@@ -23,7 +23,7 @@ from TG.helix.actors.base import HelixObject
 #~ Layout Cell
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-class MatuiCell(layouts.LayoutCell):
+class MatuiCell(HelixObject, layouts.LayoutCell):
     _fm_ = OBFactoryMap(Layout = None)
     oset = OBSet.property()
     host = None
@@ -33,6 +33,8 @@ class MatuiCell(layouts.LayoutCell):
 
     def __init__(self, host):
         self.host = host
+
+    def isLayout(self): return True
 
     def set(self, *args, **kw):
         for n,v in args:
@@ -45,15 +47,19 @@ class MatuiCell(layouts.LayoutCell):
         return self
     cell = property(getLayoutCell)
 
-    def layoutInBox(self, lbox):
-        host = self.host()
+    def watchBox(self, box):
+        box.kvo('*', self.layoutInBox)
+
+    def layoutInBox(self, lbox, k='*'):
+        lbox = lbox.copy(dim=2)
+        host = self.host
         placeFn = self.placeFn
         if placeFn is not None:
             placeFn(host, lbox)
         else:
             host.box = lbox.copy()
 
-        self.oset.call_n1(getattr(host, 'box', lbox))
+        self.oset.call_n2(self, getattr(host, 'box', lbox))
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -87,15 +93,14 @@ class MatuiCell(layouts.LayoutCell):
             host.box.inset(inset)
         return self
 
-    def newLayout(self, kind='abs'):
-        return self._fm_.Layout(kind)
-    def addLayout(self, kind='abs'):
-        layout = self.newLayout(kind)
-        layout.parentCell = self
+    def newLayout(self, kind='abs', node=None, cell=True):
+        return self._fm_.Layout(kind, node, cell)
+    def addLayout(self, kind='abs', node=None, cell=None):
+        layout = self.newLayout(kind, node, cell)
+        layout.watchCell(self)
         return layout
     def removeLayout(self, layout):
-        if layout.parentCell is self:
-            layout.parentCell = None
+        layout.watchCell(self, False)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~ Layout Object
@@ -124,11 +129,16 @@ class MatuiLayout(HelixObject, KVObject):
     strategy = KVProperty(None)
     collection = KVProperty(KVList)
     box = KVBox.property()
+    cell = None
 
-    def __init__(self, kind='abs'):
+    def __init__(self, kind='abs', node=None, cell=None):
         self.setKind(kind)
+        if node is not None:
+            self.setNode(node)
+        if cell is not None:
+            self.setCell(cell)
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def isLayout(self): return True
 
     _kind = None
     def getKind(self):
@@ -141,44 +151,59 @@ class MatuiLayout(HelixObject, KVObject):
         self.strategy = factory()
     kind = property(getKind, setKind)
 
-    def watchBox(self, box):
-        self.box.viewOf(box, dim=2)
-        self.kvo('box.*', lambda self, box: self.layout())
+    _node = None
+    def getNode(self):
+        return self._node
+    def setNode(self, node):
+        self._node = node
+    node = property(getNode, setNode)
 
-    _parentCell = None
-    def getParentCell(self):
-        return self._parentCell
-    def setParentCell(self, parentCell):
-        lastParentCell = self.getParentCell()
-        if lastParentCell is not None:
-            lastParentCell.oset.discard(self.layout)
-        self._parentCell = parentCell
-        if parentCell is not None:
-            parentCell.oset.add(self.layout)
-    parentCell = property(getParentCell, setParentCell)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def setCell(self, cell):
+        if cell is True:
+            cell = self._fm_.Cell(self)
+
+        self.watchCell(cell)
+        self.cell = cell
+
+    def watchCell(self, cell, watch=True):
+        cell.oset.change(watch, self.onWatchedCellNotify)
+
+    def onWatchedCellNotify(self, wcell, lbox):
+        self.layout(lbox)
 
     def layout(self, lbox=None):
         if lbox is not None:
             self.box.pv = lbox.pv[..., :2]
 
         self.strategy(self.collection, self.box)
-    __call__ = layout
+
+    def fit(self):
+        box = self.strategy.fit(self.collection, self.box)
+        self.box.size = box.size
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def add(self, item):
+        if not item.isLayout() and self._node is not None:
+            self._node.add(item)
         itemCell = item.cell
         self.collection.append(itemCell)
         return itemCell
 
     def remove(self, item):
+        if not item.isLayout() and self._node is not None:
+            self._node.remove(item)
         itemCell = item.cell
         if itemCell in self.collection:
             self.collection.remove(itemCell)
             return itemCell
         return None
 
-    def clear(self, collection):
+    def clear(self):
+        if self._node is not None:
+            self._node.clear()
         self.collection[:] = []
 
 MatuiCell._fm_.update(Layout = MatuiLayout)
